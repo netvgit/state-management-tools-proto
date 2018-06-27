d936e5a37304ddd890911026f6894a35e0890d9e
viveka <git-fake@git.com>
2018-06-27 10:36:55 +0530

    Initial Commit with all react + webpack + babel configuration in place

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component{
+  render() {
+    return(
+      <div>
+        Content in the first App div
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['env', 'react'] }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+}
