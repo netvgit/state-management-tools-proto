d1d1803bee7df8ff027aeb03514bcc3a04d9349e
viveka <git-fake@git.com>
2018-06-27 11:23:08 +0530

    Added the initial components and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from './components/Home';
+import Users from './components/Users';
+import Books from './components/Books';
+import UsersBooks from './components/UsersBooks';
+
 import './App.css';
 
-class App extends Component{
+class App extends Component {
   render() {
-    return(
+    return (
       <div>
-        Content in the first App div
+        <Router>
+          <div>
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/users">Users</Link></li>
+                <li><Link to="/books">Books</Link></li>
+                <li><Link to="/users-books">Users Books</Link></li>
+              </ul>
+            </div>
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route path="/users" component={Users} />
+              <Route path="/books" component={Books} />
+              <Route path="/users-books" component={UsersBooks} />
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/UsersBooks.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UsersBooks extends Component {
+  render() {
+    return (
+      <div>
+        Content UsersBooks
+      </div>
+    );
+  }
+}
+
+export default UsersBooks;
